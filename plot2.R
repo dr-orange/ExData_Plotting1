28e616e4b3fc7fd7eb543909a6184bac09d6f20a
Koji <dr.orange.jr@gmail.com>
2018-07-03 02:59:31 +0900

    first commit

---
+++ plot2.R
@@ -0,0 +1,42 @@
+library(dplyr)
+library(stringr) # str_c()
+library(lubridate) # dmy_hms()
+
+# Output the day of the week in English
+Sys.setlocale(category = "LC_TIME", locale = "C")
+
+rawDataFileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+workingDataPath <- file.path(".", "data")
+downloadedZipfilePath <- file.path(workingDataPath, "downloaded_dataset.zip")
+rawDataPath <- file.path(workingDataPath, "household_power_consumption.txt")
+# Create working directory
+if(!file.exists(workingDataPath)) {
+        dir.create(workingDataPath)
+}
+# Download ziped file
+if(!file.exists(downloadedZipfilePath)) {
+        download.file(rawDataFileUrl, destfile = downloadedZipfilePath)
+}
+# Unzip
+if(!file.exists(rawDataPath)) {
+        unzip(zipfile = downloadedZipfilePath, exdir = workingDataPath)
+}
+# Read Data
+dataset <- read.table(rawDataPath, header = TRUE, sep = ";", na.strings = "?",
+                      colClasses = c("character", "character", rep("numeric", 7))) %>%
+        # Using data from the dates 2007-02-01 and 2007-02-02
+        filter(Date == "1/2/2007" | Date == "2/2/2007") %>%
+        # Joining day and time to create a new date/time
+        mutate(DateTimeText = str_c(Date, Time, sep = " "), 
+               Time = dmy_hms(DateTimeText)) %>%
+        select(-DateTimeText, -Date) %>%
+        rename(DateTime = Time)
+
+# Device ready
+png("plot2.png", width = 480, height = 480)
+
+plot(dataset$DateTime, dataset$Global_active_power, type = "l", col = "black",
+     xlab = "", ylab = "Global Active Power (kilowatts)")
+
+# Device off
+dev.off()
